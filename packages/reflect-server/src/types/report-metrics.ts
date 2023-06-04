cbb74cf4c671b2c3b9a0c5bdbc8dde3fba6dee7a
Darick <oceans@gmail.com>
2023-06-03 18:10:37 -0700

    More precise union type for ReportMetrics

--- packages/reflect-server/src/types/report-metrics.ts
+++ packages/reflect-server/src/types/report-metrics.ts
@@ -1,34 +1,40 @@
 import * as v from 'shared/valita.js';
 
-export const pointSchema = v.union(
-  // count, rate, gauge
-  v.tuple([v.number(), v.number()]),
-  // distribution
-  v.tuple([v.number(), v.array(v.number())]),
-);
+export const pointSchema = v.tuple([v.number(), v.number()]);
+export const pointsSchema = v.tuple([v.number(), v.array(v.number())]);
+
 export type Point = v.Infer<typeof pointSchema>;
+export type Points = v.Infer<typeof pointsSchema>;
 
 // https://docs.datadoghq.com/api/latest/metrics/#submit-metrics (v1)
 export const COUNT_METRIC_TYPE = 'count';
 export const RATE_METRIC_TYPE = 'rate';
 export const GAUGE_METRIC_TYPE = 'gauge';
-// https://docs.datadoghq.com/api/latest/metrics/#submit-distribution-points
-export const DISTRIBUTION_METRIC_TYPE = 'distribution';
-
-export const metricsTypeSchema = v.union(
-  v.literal(COUNT_METRIC_TYPE),
-  v.literal(RATE_METRIC_TYPE),
-  v.literal(GAUGE_METRIC_TYPE),
-  v.literal(DISTRIBUTION_METRIC_TYPE),
-);
-
-export const seriesSchema = v.object({
+export const nonDistributionSchema = v.object({
   host: v.string().optional(),
   metric: v.string(),
   points: v.array(pointSchema),
   tags: v.array(v.string()).optional(),
-  type: metricsTypeSchema.optional(),
+  type: v
+    .union(
+      v.literal(COUNT_METRIC_TYPE),
+      v.literal(RATE_METRIC_TYPE),
+      v.literal(GAUGE_METRIC_TYPE),
+    )
+    .optional(),
 });
+
+// https://docs.datadoghq.com/api/latest/metrics/#submit-distribution-points
+export const DISTRIBUTION_METRIC_TYPE = 'distribution';
+export const distributionSchema = v.object({
+  host: v.string().optional(),
+  metric: v.string(),
+  points: v.array(pointsSchema),
+  tags: v.array(v.string()).optional(),
+  type: v.literal(DISTRIBUTION_METRIC_TYPE).optional(),
+});
+
+export const seriesSchema = v.union(distributionSchema, nonDistributionSchema);
 export type Series = v.Infer<typeof seriesSchema>;
 
 export const reportMetricsSchema = v.object({
