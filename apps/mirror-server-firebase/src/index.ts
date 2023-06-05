8750eb345f289795b7c713f1cbe595f5abf54f15
Cesar Alaestante <cesar@roci.dev>
2023-06-05 14:54:17 -0700

    remove express

--- apps/mirror-server-firebase/src/index.ts
+++ apps/mirror-server-firebase/src/index.ts
@@ -1,48 +1,21 @@
 import * as functions from 'firebase-functions';
 import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import type Express from 'express';
 import {functionsConfig} from './functions-config.js';
-import {handleRequest} from './handle-request.js';
+import {publish as p} from './functions/publish.function.js';
+import {heartbeat as h} from './functions/heartbeat.function.js';
+
 // CORS configuration.
 const options: cors.CorsOptions = {
   origin: functionsConfig.whitelist,
 };
 
+const withCors = (fn) => {
+  return functions.https.onRequest((req, res) => {
+    cors(options)(req, res, () => {
+      fn(req, res);
+    });
+  });
+}
 
-
-const VERSION = 'v1';
-const app = express();
-
-app.use(helmet());
-app.use(cors(options));
-
-
-app.post(
-  `/${VERSION}/mirror/:op`,
-  async (
-    req: Express.Request,
-    res: Express.Response,
-    next: Express.NextFunction,
-  ) => {
-    await handleRequest(req, res, next);
-  },
-);
-
-
-
-
-
-app.get(
-  `/${VERSION}/mirror/heartbeat`,
-  async (req: Express.Request, res: Express.Response) => {
-    res.status(200).send('OK');
-  },
-);
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
-
-export const api = functions.https.onRequest(app);
+export const publish = withCors(p);
+export const heartbeat = withCors(h);
