90ee77d2d5481aba4fca18c936daf67d1ef0c220
Cesar Alaestante <cesar@roci.dev>
2023-06-14 13:58:01 -0700

    format

--- mirror/reflect-auth-ui/config/firebaseApp.config.ts
+++ mirror/reflect-auth-ui/config/firebaseApp.config.ts
@@ -1,4 +1,3 @@
-
 export const firebaseConfig = {
   apiKey: 'AIzaSyDxHw3_wWcLkpjWgprfEPhrppFr3SgV03M',
   authDomain: 'reflect-mirror-staging.firebaseapp.com',
@@ -7,5 +6,3 @@ export const firebaseConfig = {
   messagingSenderId: '709901628211',
   appId: '1:709901628211:web:8164d4a5cd28226a180446',
 };
-
-
