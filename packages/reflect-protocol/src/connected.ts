39dc3fd9806203b5d00324ac39d809cc2d15852d
Gregory Baker <greg@roci.dev>
2023-06-14 10:36:11 -0700

    format fix

--- packages/reflect-protocol/src/connected.ts
+++ packages/reflect-protocol/src/connected.ts
@@ -2,7 +2,7 @@ import * as v from 'shared/valita.js';
 
 export const connectedBodySchema = v.object({
   wsid: v.string(),
-  timestamp: v.number().optional()
+  timestamp: v.number().optional(),
 });
 
 export const connectedMessageSchema = v.tuple([
