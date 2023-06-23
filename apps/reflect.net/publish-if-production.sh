0d3901544306651f1d1ebea8a171511da7260d98
Gregory Baker <greg@roci.dev>
2023-06-22 22:29:20 -0700

    fix: unblock builds by removing wrangler publish from vercel deploy

--- apps/reflect.net/publish-if-production.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z "$VERCEL_PRODUCTION_BUILD" ]
-then
-  echo "Not a production build pushing staging worker"
-  npm run publish-worker-staging
-else
-  echo "Production build pushing prod worker"
-  npm run publish-worker-prod
-fi
