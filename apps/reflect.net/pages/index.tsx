d76f7f78053dcd94eb9d019df769e919e1dbadf5
Alexander King <alexhking@gmail.com>
2023-06-27 12:16:23 -0400

    Attempting to fix failing test

--- apps/reflect.net/pages/index.tsx
+++ apps/reflect.net/pages/index.tsx
@@ -120,8 +120,8 @@ export default function Home() {
       </Head>
 
       <div itemScope itemType="https://schema.org/WebSite">
-        <meta itemProp="url" content="https://reflect.net/"/>
-        <meta itemProp="name" content="Reflect"/>
+        <meta itemProp="url" content="https://reflect.net/" />
+        <meta itemProp="name" content="Reflect" />
       </div>
 
       <Nav gameMode={gameMode} />
