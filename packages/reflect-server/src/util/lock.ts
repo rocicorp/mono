cbdd1a880c0b125dc0f948369f14bb7ce19952dd
Darick <oceans@gmail.com>
2023-07-26 17:02:35 -0700

    dynamic log level

--- packages/reflect-server/src/util/lock.ts
+++ packages/reflect-server/src/util/lock.ts
@@ -11,53 +11,57 @@ export class LoggingLock {
     lc: LogContext,
     name: string,
     fn: () => MaybePromise<void>,
-    flushLogsIfLockHeldForMs = 1000,
+    flushLogsIfLockHeldForMs = 100,
   ): Promise<void> {
-    const t0 = Date.now();
     this.#waiters.push(name);
+    lc = lc.withContext('function', name);
 
     if (this.#waiters.length > 1) {
-      lc.debug?.(
+      // Flush the log if the number of waiters is a multiple of 5.
+      const flush = this.#waiters.length % 5 === 0;
+
+      (flush ? lc.info : lc.debug)?.(
         `${name} waiting for ${this.#holder} with ${
           this.#waiters.length - 1
         } other waiter(s): ${this.#waiters}`,
       );
-      if (this.#waiters.length % 5 === 0) {
-        // Flush the log if the number of waiters is a multiple of 5.
+      if (flush) {
         await lc.flush();
       }
     }
 
-    let flushLogs = false;
+    let flushAfterLock = false;
+    const t0 = Date.now();
 
     await this.#lock.withLock(async () => {
       const t1 = Date.now();
+
       this.#waiters.splice(this.#waiters.indexOf(name), 1);
       this.#holder = name;
       const elapsed = t1 - t0;
       if (elapsed > 0) {
-        lc.withContext('timing', 'lock-acquired')
-          .withContext('function', name)
-          .debug?.(`${name} acquired lock in ${elapsed} ms`);
+        lc.withContext('timing', 'lock-acquired').debug?.(
+          `${name} acquired lock in ${elapsed} ms`,
+        );
       }
 
       try {
         await fn();
       } finally {
         const t2 = Date.now();
+
         this.#holder = undefined;
         const elapsed = t2 - t1;
         if (elapsed > 0) {
-          lc.withContext('timing', 'lock-held')
-            .withContext('function', name)
-            .debug?.(`${name} held lock for ${elapsed} ms`);
-          if (elapsed >= flushLogsIfLockHeldForMs) {
-            flushLogs = true; // Flush after releasing the lock.
-          }
+          flushAfterLock = elapsed >= flushLogsIfLockHeldForMs;
+          lc = lc.withContext('timing', 'lock-held');
+          (flushAfterLock ? lc.info : lc.debug)?.(
+            `${name} held lock for ${elapsed} ms`,
+          );
         }
       }
     });
-    if (flushLogs) {
+    if (flushAfterLock) {
       await lc.flush();
     }
   }
