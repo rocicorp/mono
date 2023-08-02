b61179612232d7f3e6a81c202781b41955c51f0e
Gregory Baker <greg@roci.dev>
2023-08-01 21:38:28 -0700

    Revert "Revert "chore: More major logging fixes.""
    This reverts commit 05654f3a3b4d76e13dbe51b5be76aecc4d15a453.

--- packages/reflect-server/src/server/worker.ts
+++ packages/reflect-server/src/server/worker.ts
@@ -25,7 +25,13 @@ import {
   withBody,
 } from './router.js';
 import {withUnhandledRejectionHandler} from './unhandled-rejection-handler.js';
+<<<<<<< HEAD
 import {timed} from 'shared/src/timed.js';
+=======
+import type {MaybePromise} from 'replicache';
+import {version} from '../mod.js';
+import {populateLogContextFromRequest} from '../util/log-context-common.js';
+>>>>>>> parent of 05654f3a (Revert "chore: More major logging fixes.")
 
 export type MetricsSink = (
   allSeries: Series[],
@@ -154,6 +160,7 @@ export function createWorker<Env extends BaseWorkerEnv>(
         ctx,
         logSink,
         logLevel,
+        request,
         withUnhandledRejectionHandler(lc =>
           fetch(request, env, router, lc, datadogMetricsOptions),
         ),
@@ -169,6 +176,7 @@ export function createWorker<Env extends BaseWorkerEnv>(
         ctx,
         logSink,
         logLevel,
+        undefined,
         withUnhandledRejectionHandler(lc => scheduled(env, lc)),
       );
     },
@@ -286,12 +294,16 @@ async function withLogContext<R>(
   ctx: ExecutionContext,
   logSink: LogSink,
   logLevel: LogLevel,
+  req: Request | undefined,
   fn: (lc: LogContext) => Promise<R>,
 ): Promise<R> {
-  const lc = new LogContext(logLevel, undefined, logSink).withContext(
+  let lc = new LogContext(logLevel, undefined, logSink).withContext(
     'component',
     'Worker',
   );
+  if (req !== undefined) {
+    lc = populateLogContextFromRequest(lc, req);
+  }
   try {
     return await fn(lc);
   } finally {
