d81c57328531a6668a15d5ceb8e3242a70a89efd
Gregory Baker <greg@roci.dev>
2023-08-07 19:28:58 -0700

    Revert "feat!: backfill the version index for FF (#733)"
    This reverts commit 64f0dc729695fa97f1637a39a16891d9d7d6e7a0.

--- packages/reflect-server/src/server/room-schema.ts
+++ packages/reflect-server/src/server/room-schema.ts
@@ -1,14 +1,9 @@
 import type {LogContext} from '@rocicorp/logger';
 import type {DurableStorage} from '../storage/durable-storage.js';
 import {VersionMigrationMap, initStorageSchema} from '../storage/schema.js';
-import {backfillVersionIndex} from '../ff/backfill-version-index.js';
 
 const ROOM_VERSION_MIGRATION_MAP: VersionMigrationMap = {
-  // The inaugural v1 understands the rollback limit.
-  1: {minSafeRollbackVersion: 1},
-
-  // Initialize / fix version index for Fast(er) Forward.
-  2: backfillVersionIndex,
+  1: {minSafeRollbackVersion: 1}, // The inaugural v1 understands the rollback limit.
 };
 
 export async function initRoomSchema(
