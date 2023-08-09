c154c7355b94f9de2d0f42a95c4b7d1f5042e8e8
Erik Arvidsson <arv@roci.dev>
2023-08-09 15:06:25 +0200

    more

--- mirror/mirror-server/src/functions/tail/create.handler.ts
+++ mirror/mirror-server/src/functions/tail/create.handler.ts
@@ -4,25 +4,22 @@ import {onRequest} from 'firebase-functions/v2/https';
 
 export const create = (_firestore: Firestore, _auth: Auth) =>
   onRequest(async (request, response) => {
+    console.log(typeof request.body);
+    // TODO(arv): Validate request.body
+    // TODO(arv): userAuthorization()
+    // TODO(arv): appAuthorization()
+
     response.writeHead(200, {
       'Cache-Control': 'no-store',
       'Content-Type': 'text/event-stream',
-      // 'Transfer-Encoding': 'chunked',
     });
-    response.flushHeaders();
+
+    // const {tail, expiration, deleteTail} = await createTail(apiToken, accountID, cfWorkerName, filters, debug, env, packageVersion)
+
     console.log(`request.url: ${request.url}\n`);
-    for (let i = 0; i < 100; i++) {
-      await new Promise<void>((resolve, reject) => {
-        console.log(`Writing Item ${i}`);
-        response.write(`data: Item ${i}\n\n`, err => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        });
-      });
-      await new Promise(resolve => setTimeout(resolve, 100));
+    for (let i = 0; i < 10; i++) {
+      response.write(`data: Item ${i}\n\n`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
     }
     response.end();
   });
