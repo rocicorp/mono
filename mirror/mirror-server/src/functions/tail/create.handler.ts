3c623b6f35facf944fdbae4e81e5a4e871ed0e18
Erik Arvidsson <arv@roci.dev>
2023-08-09 14:32:25 +0200

    Use correct data format and headers

--- mirror/mirror-server/src/functions/tail/create.handler.ts
+++ mirror/mirror-server/src/functions/tail/create.handler.ts
@@ -5,14 +5,16 @@ import {onRequest} from 'firebase-functions/v2/https';
 export const create = (_firestore: Firestore, _auth: Auth) =>
   onRequest(async (request, response) => {
     response.writeHead(200, {
-      'Content-Type': 'text/plain',
+      'Cache-Control': 'no-store',
+      'Content-Type': 'text/event-stream',
+      // 'Transfer-Encoding': 'chunked',
     });
     response.flushHeaders();
     console.log(`request.url: ${request.url}\n`);
     for (let i = 0; i < 100; i++) {
       await new Promise<void>((resolve, reject) => {
-        console.log(`Writing Item ${i}\n`);
-        response.write(`Item ${i}\n`, err => {
+        console.log(`Writing Item ${i}`);
+        response.write(`data: Item ${i}\n\n`, err => {
           if (err) {
             reject(err);
           } else {
