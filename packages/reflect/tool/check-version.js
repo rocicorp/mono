fd82df729b8a77889dd78961b9086574434a0df8
Erik Arvidsson <arv@roci.dev>
2023-09-11 12:22:37 +0200

    chore: Fix check-version script
    This actually broke in a26d2a69ab387a49a3f369482d965a941f68b49f but it
    needed further work due to this PR.

--- packages/reflect/tool/check-version.js
+++ packages/reflect/tool/check-version.js
@@ -1,8 +1,12 @@
 // @ts-check
 
 import {readFileSync} from 'node:fs';
-import * as path from 'path';
+import {opendir} from 'node:fs/promises';
 import {fileURLToPath} from 'node:url';
+import * as path from 'path';
+import colors from 'picocolors';
+
+const {bold} = colors;
 
 /**
  * @param {string} fileName
@@ -19,16 +23,37 @@ function read(fileName) {
  * @param {string} fileName
  * @param {string} version
  */
-function checkFileForVersion(fileName, version) {
-  const js = read(fileName);
-  if (!new RegExp('var [a-zA-Z0-9]+ ?= ?"' + version + '";', 'g').test(js)) {
-    console.error(`Did not find expected version ${version} in ${fileName}`);
-    process.exit(1);
+function fileContainsVersionString(fileName, version) {
+  const js = readFileSync(fileName, 'utf-8');
+  return new RegExp('var [a-zA-Z0-9]+ ?= ?"' + version + '";', 'g').test(js);
+}
+
+/**
+ * Checks that there is a file in dir that contains the version string
+ * @param {string} dir
+ * @param {string} version
+ */
+async function checkFilesForVersion(dir, version) {
+  /** @type string[] */
+  const files = [];
+  for await (const entry of await opendir(dir)) {
+    if (!entry.isFile() || !entry.name.endsWith('.js')) {
+      continue;
+    }
+    if (fileContainsVersionString(path.join(dir, '/', entry.name), version)) {
+      return;
+    }
+    files.push(entry.name);
   }
+
+  console.error(
+    `Version string ${bold(version)} not found in any of these files in ${bold(
+      dir,
+    )} dir:\n  ${files.join('\n  ')}`,
+  );
+  process.exit(1);
 }
 
 const {version} = JSON.parse(read('package.json'));
 
-checkFileForVersion('index.js', version);
-checkFileForVersion('client.js', version);
-checkFileForVersion('server.js', version);
+checkFilesForVersion('out', version);
