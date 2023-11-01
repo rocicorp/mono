f12767a5815f559c414a0c5d39ad7a7de3272ab9
Erik Arvidsson <arv@roci.dev>
2023-11-01 21:31:35 +0100

    restore

--- packages/shared/src/tool/get-external-from-package-json.js
+++ packages/shared/src/tool/get-external-from-package-json.js
@@ -4,61 +4,68 @@
 
 import {readFile} from 'node:fs/promises';
 import {createRequire} from 'node:module';
+import {fileURLToPath} from 'node:url';
 import {pkgUp} from 'pkg-up';
-import {isInternalPackage} from './internal-packages.js';
 
 /**
  * @param {string} basePath
  * @returns {Promise<string[]>}
  */
 export async function getExternalFromPackageJSON(basePath) {
-  const result = new Set();
-  await getExternalFromPackageJSONInternal(basePath, new Set(), result);
-  return [...result];
-}
-
-/**
- * @param {string} basePath
- * @param {Set<string>} visited
- * @param {Set<string>} result
- * @returns {Promise<void>}
- */
-async function getExternalFromPackageJSONInternal(basePath, visited, result) {
   const path = await pkgUp({cwd: basePath});
   if (!path) {
     throw new Error('Could not find package.json');
   }
-  const pkg = JSON.parse(await readFile(path, 'utf-8'));
+  const x = await readFile(path, 'utf-8');
+  const pkg = JSON.parse(x);
 
-  if (visited.has(pkg.name)) {
-    return;
-  }
-  visited.add(pkg.name);
-
+  const deps = new Set();
   for (const dep of Object.keys({
     ...pkg.dependencies,
     ...pkg.peerDependencies,
   })) {
     if (isInternalPackage(dep)) {
-      await getRecursiveExternals(dep, visited, result);
+      for (const depDep of await getRecursiveExternals(dep)) {
+        deps.add(depDep);
+      }
     } else {
-      result.add(dep);
+      deps.add(dep);
     }
   }
+  return [...deps];
+}
+
+const internalPackages = [
+  'cloudflare-api',
+  'datadog',
+  'mirror-protocol',
+  'mirror-schema',
+  'reflect-client',
+  'reflect-protocol',
+  'reflect-server',
+  'reflect-shared',
+  'reflect-react',
+  'shared',
+  'reflect-cli',
+];
+
+/**
+ * @param {string} name
+ */
+function isInternalPackage(name) {
+  // A better solution might be to check if the package is in the monorepo
+  return internalPackages.includes(name);
 }
 
 /**
  * @param {string} name
- * @param {Set<string>} visited
- * @param {Set<string>} result
  */
-async function getRecursiveExternals(name, visited, result) {
+function getRecursiveExternals(name) {
   if (name === 'shared') {
-    await getExternalFromPackageJSONInternal(import.meta.url, visited, result);
-    return;
+    return getExternalFromPackageJSON(fileURLToPath(import.meta.url));
   }
 
   const require = createRequire(import.meta.url);
   const depPath = require.resolve(name);
-  await getExternalFromPackageJSONInternal(depPath, visited, result);
+  return getExternalFromPackageJSON(depPath);
 }
