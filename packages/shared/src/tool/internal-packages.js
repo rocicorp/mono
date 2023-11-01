a19f0a6adec6705f7b6a3ec12ae0caa480829564
Erik Arvidsson <arv@roci.dev>
2023-11-01 17:54:07 +0100

    fix lint

--- packages/shared/src/tool/internal-packages.js
+++ packages/shared/src/tool/internal-packages.js
@@ -1,3 +1,7 @@
+// @ts-check
+
+/* eslint-env node, es2022 */
+
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import {fileURLToPath} from 'node:url';
@@ -26,6 +30,9 @@ for (const p of ['packages', 'mirror']) {
 
 export const internalPackages = [...internalPackagesMap.keys()];
 
+/**
+ * @param {string} name
+ */
 export function isInternalPackage(name) {
   return internalPackagesMap.has(name);
 }
