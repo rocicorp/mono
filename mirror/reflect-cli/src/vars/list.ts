a21a83ab6548758f39bdaac7747efa658dff706e
Darick <132324914+d-llama@users.noreply.github.com>
2023-11-03 17:40:35 -0700

    feat(reflect-cli|mirror): update vars-list protocol to support multiple envs

--- mirror/reflect-cli/src/vars/list.ts
+++ mirror/reflect-cli/src/vars/list.ts
@@ -1,8 +1,10 @@
-import {listVars} from 'mirror-protocol/src/vars.js';
+import {Env, listVars} from 'mirror-protocol/src/vars.js';
+import color from 'picocolors';
 import {ensureAppInstantiated} from '../app-config.js';
 import {authenticate} from '../auth-config.js';
 import {listDevVars} from '../dev/vars.js';
 import {makeRequester} from '../requester.js';
+import {padColumns} from '../table.js';
 import type {YargvToInterface} from '../yarg-types.js';
 import type {CommonVarsYargsArgv} from './types.js';
 
@@ -25,33 +27,48 @@ export async function listVarsHandler(
     $0: command,
     _: [subcommand],
   } = yargs;
-  let vars: Record<string, string>;
+  let envs: Record<string, Env>;
   if (dev) {
-    vars = listDevVars();
+    envs = {
+      dev: {
+        name: 'Local dev',
+        vars: listDevVars(),
+      },
+    };
   } else {
     const {userID} = await authenticate(yargs);
     const {appID} = await ensureAppInstantiated(yargs);
     const data = {requester: makeRequester(userID), appID, decrypted: show};
 
     const response = await listVars(data);
-    vars = response.vars;
+    envs = response.envs;
   }
-  const varType = dev ? 'local dev' : 'environment';
-  const entries = Object.entries(vars);
-  if (entries.length === 0) {
-    console.log(
-      `No ${varType} variables set. Use '${command} ${subcommand} set${
-        dev ? ' --dev' : ''
-      }' to add them.`,
-    );
-  } else if (dev) {
-    console.log(`Local dev variables:\n`);
-  } else if (show) {
-    console.log(`Requested decrypted environment variables:\n`);
-  } else {
-    console.log(
-      'Environment variables are encrypted at rest. Use --show to see their values.\n',
-    );
+  for (const env of Object.values(envs)) {
+    const entries = Object.entries(env.vars);
+    if (entries.length === 0) {
+      console.log(
+        `No variables set. Use '${command} ${subcommand} set${
+          dev ? ' --dev' : ''
+        }' to add them.`,
+      );
+      continue;
+    }
+    const showHelp = dev || show ? '' : ' (use --show to see their values)';
+    if (showHelp) {
+      entries.forEach(entry => {
+        entry[1] = color.italic(color.gray('Encrypted'));
+      });
+    }
+    console.log(`\n${env.name ?? 'Live'} environment variables${showHelp}:\n`);
+
+    const lines = padColumns([['name', 'value'], ...entries]);
+    lines.forEach(([key, value], i) => {
+      if (i === 0) {
+        // Header row
+        console.log(`${color.gray(key)}     ${color.gray(value)}`);
+      } else {
+        console.log(`${color.bold(key)}     ${value}`);
+      }
+    });
   }
-  entries.forEach(([name, value]) => console.log(`${name}=${value}`));
 }
