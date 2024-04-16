1f4e44d1869be218d3bd8a819f7a77ec87818408
Darick Tong <d@roci.dev>
2024-04-15 18:04:58 -0700

    moar scaffolding

--- packages/zero-cache/src/services/registry.ts
+++
@@ -1,8 +0,0 @@
-import type {Replicator} from './replicator/replicator.js';
-import type {ViewSyncer} from './view-syncer/view-syncer.js';
-
-export interface ServiceRegistry {
-  getReplicator(): Replicator;
-
-  getViewSyncer(id: string): ViewSyncer;
-}
