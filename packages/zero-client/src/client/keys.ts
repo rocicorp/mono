6661a55cc6c5bf4a500b1f5a059e5f2d8956aa05
Gregory Baker <greg@roci.dev>
2024-04-26 13:06:58 -0700

    feat(zero-client,zero-protocol): composite key support at protocol and poke handler level

--- packages/zero-client/src/client/keys.ts
+++ packages/zero-client/src/client/keys.ts
@@ -1,3 +1,6 @@
+import {assert} from 'shared/src/asserts.js';
+import type {EntityID} from 'zero-protocol/src/entity.js';
+
 export const CLIENTS_KEY_PREFIX = 'c/';
 export const DESIRED_QUERIES_KEY_PREFIX = 'd/';
 export const GOT_QUERIES_KEY_PREFIX = 'g/';
@@ -15,6 +18,13 @@ export function toGotQueriesKey(hash: string): string {
   return GOT_QUERIES_KEY_PREFIX + hash;
 }
 
-export function toEntitiesKey(key: string): string {
-  return ENTITIES_KEY_PREFIX + key;
+export function toEntitiesKey(entityType: string, entityID: EntityID): string {
+  const entries = Object.entries(entityID);
+  assert(entries.length > 0);
+  entries.sort(([keyA], [keyB]) => (keyA < keyB ? -1 : 1));
+  let idSegment = entries[0][1];
+  for (let i = 1; i < entries.length; i++) {
+    idSegment += '_' + entries[i][1];
+  }
+  return ENTITIES_KEY_PREFIX + entityType + '/' + idSegment;
 }
