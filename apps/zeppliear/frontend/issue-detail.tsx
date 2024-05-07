077ab40d05c8fbe34b309869b62a68fe17f8b83a
Gregory Baker <greg@roci.dev>
2024-05-07 15:56:14 -0700

    try not doing an alias

--- apps/zeppliear/frontend/issue-detail.tsx
+++ apps/zeppliear/frontend/issue-detail.tsx
@@ -31,17 +31,17 @@ interface Props {
 }
 
 function CommentsList(
-  comments: {comment: Comment; creator: {name: string}}[],
+  comments: {comment: Comment; member: {name: string}}[],
   isLoading: boolean,
 ) {
-  const elements = comments.map(({comment, creator}) => (
+  const elements = comments.map(({comment, member}) => (
     <div
       key={comment.id}
       className="max-w-[85vw] mx-3 bg-gray-850 mt-0 mb-5 border-transparent rounded py-3 px-3 relative whitespace-pre-wrap overflow-auto"
     >
       <div className="h-6 mb-1 -mt-px relative">
         <DefaultAvatarIcon className="w-4.5 h-4.5 rounded-full overflow-hidden flex-shrink-0 float-left mr-2" />
-        {creator.name} {timeAgo(comment.created)}
+        {member.name} {timeAgo(comment.created)}
       </div>
       <div className="block flex-1 whitespace-pre-wrap">
         <Remark>{comment.body}</Remark>
@@ -111,14 +111,14 @@ export default function IssueDetail({
   const comments = useQuery(
     commentQuery
       .where('issueID', '=', detailIssueID ?? '')
-      .join(zero.query.member, 'creator', 'comment.creatorID', 'member.id')
+      .join(zero.query.member, 'member', 'comment.creatorID', 'member.id')
       .select(
         'comment.id',
         'comment.issueID',
         'comment.created',
         'comment.creatorID',
         'comment.body',
-        'creator.name',
+        'member.name',
       )
       .asc('comment.created'),
     [detailIssueID],
