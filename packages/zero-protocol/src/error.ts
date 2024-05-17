1746284f81bc815659779439818ac5103bf23afb
Gregory Baker <greg@roci.dev>
2024-05-17 11:20:29 -0700

    feat(zero-cache): ErrorForClient for throwing errors that the client should be informed of

--- packages/zero-protocol/src/error.ts
+++ packages/zero-protocol/src/error.ts
@@ -4,6 +4,7 @@ import * as v from 'shared/src/valita.js';
 // so if you add or change on here a corresponding dashboard
 // change will likely be needed.
 
+<<<<<<< HEAD
 export const errorKindSchema = v.union(
   v.literal('AuthInvalidated'),
   v.literal('ClientNotFound'),
@@ -18,8 +19,37 @@ export const errorKindSchema = v.union(
   v.literal('Unknown'),
   v.literal('VersionNotSupported'),
 );
+=======
+export enum ErrorKind {
+  AuthInvalidated = 'AuthInvalidated',
+  ClientNotFound = 'ClientNotFound',
+  InvalidConnectionRequest = 'InvalidConnectionRequest',
+  InvalidConnectionRequestBaseCookie = 'InvalidConnectionRequestBaseCookie',
+  InvalidConnectionRequestLastMutationID = 'InvalidConnectionRequestLastMutationID',
+  InvalidConnectionRequestClientDeleted = 'InvalidConnectionRequestClientDeleted',
+  InvalidMessage = 'InvalidMessage',
+  InvalidPush = 'InvalidPush',
+  MutationFailed = 'MutationFailed',
+  Unauthorized = 'Unauthorized',
+  VersionNotSupported = 'VersionNotSupported',
+  Internal = 'Internal',
+}
+>>>>>>> 58871f74e (feat(zero-cache): ErrorForClient for throwing errors that the client should be informed of)
 
-export type ErrorKind = v.Infer<typeof errorKindSchema>;
+export const errorKindSchema: v.Type<ErrorKind> = v.union(
+  v.literal(ErrorKind.AuthInvalidated),
+  v.literal(ErrorKind.ClientNotFound),
+  v.literal(ErrorKind.InvalidConnectionRequest),
+  v.literal(ErrorKind.InvalidConnectionRequestBaseCookie),
+  v.literal(ErrorKind.InvalidConnectionRequestLastMutationID),
+  v.literal(ErrorKind.InvalidConnectionRequestClientDeleted),
+  v.literal(ErrorKind.InvalidMessage),
+  v.literal(ErrorKind.InvalidPush),
+  v.literal(ErrorKind.MutationFailed),
+  v.literal(ErrorKind.Unauthorized),
+  v.literal(ErrorKind.VersionNotSupported),
+  v.literal(ErrorKind.Internal),
+);
 
 export const errorMessageSchema = v.tuple([
   v.literal('error'),
