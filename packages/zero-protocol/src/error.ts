631694710250b30005a105d8280c5aa26159caf2
Gregory Baker <greg@roci.dev>
2024-05-22 12:19:54 -0700

    feat(replicache,zero-client): add support for disabling client group forking and use in zero-client

--- packages/zero-protocol/src/error.ts
+++ packages/zero-protocol/src/error.ts
@@ -21,11 +21,9 @@ export enum ErrorKind {
 
 export const errorKindSchema = v.union(
   v.literal(ErrorKind.AuthInvalidated),
-  v.literal(ErrorKind.ClientNotFound),
   v.literal(ErrorKind.InvalidConnectionRequest),
   v.literal(ErrorKind.InvalidConnectionRequestBaseCookie),
   v.literal(ErrorKind.InvalidConnectionRequestLastMutationID),
-  v.literal(ErrorKind.InvalidConnectionRequestClientDeleted),
   v.literal(ErrorKind.InvalidMessage),
   v.literal(ErrorKind.InvalidPush),
   v.literal(ErrorKind.MutationFailed),
