da9942d53e2588ded6d0f26835d5a8f291855ec5
Erik Arvidsson <arv@roci.dev>
2024-06-05 14:15:50 +0200

    ts-ignore node:crypto import

--- packages/shared/src/crypto.ts
+++ packages/shared/src/crypto.ts
@@ -5,7 +5,9 @@
 const localCrypto =
   typeof crypto !== 'undefined'
     ? crypto
-    : ((await import('crypto')).webcrypto as Crypto);
+    : // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore Allow non resolved import so this compiles in non node environments
+      ((await import('node:crypto')).webcrypto as Crypto);
 
 export function getRandomValues<T extends ArrayBufferView | null>(array: T): T {
   return localCrypto.getRandomValues(array);
