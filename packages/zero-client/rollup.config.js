dce5ea9f9d2fff172bfabdb7c4e712a36ac6b99d
noamackerman <noamacker@gmail.com>
2024-07-18 23:49:35 +0300

    delete mirror + zero

--- packages/zero-client/rollup.config.js
+++
@@ -1,6 +0,0 @@
-import {makeInputOptions} from 'shared/src/tool/rollup-dts.js';
-
-export default makeInputOptions(
-  'out/.dts/zero-client/src/mod.d.ts',
-  'out/zero-client.d.ts',
-);
--- packages/zql/rollup.config.js
+++
@@ -1,3 +0,0 @@
-import {makeInputOptions} from 'shared/src/tool/rollup-dts.js';
-
-export default makeInputOptions('out/.dts/zql/src/index.d.ts', 'out/zql.d.ts');
