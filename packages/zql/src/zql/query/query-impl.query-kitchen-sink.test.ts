d494d0d2432c9c18566fc2a746778a6b9a045b49
Cesar Alaestante <cesar@roci.dev>
2024-09-02 10:02:45 -0700

    fix test

--- packages/zql/src/zql/query/query-impl.query-kitchen-sink.test.ts
+++ packages/zql/src/zql/query/query-impl.query-kitchen-sink.test.ts
@@ -1,71 +1,23 @@
 import {describe, expect, test} from 'vitest';
-import {newQuery} from './query-impl.js';
-import {MemoryStorage} from '../ivm2/memory-storage.js';
-import {MemorySource} from '../ivm2/memory-source.js';
-import {
-  commentSchema,
-  issueLabelSchema,
-  issueSchema,
-  labelSchema,
-  revisionSchema,
-  userSchema,
-} from './test/testSchemas.js';
-import {toInputArgs} from './schema.js';
-import {must} from 'shared/src/must.js';
-import {Host} from '../builder/builder.js';
-import {SubscriptionDelegate} from '../context/context.js';
+import {newQuery, QueryDelegate} from './query-impl.js';
+import {issueSchema} from './test/testSchemas.js';
+import {QueryDelegateImpl} from './query-impl.query.test.js';
 
-function makeSources() {
-  const userArgs = toInputArgs(userSchema);
-  const issueArgs = toInputArgs(issueSchema);
-  const commentArgs = toInputArgs(commentSchema);
-  const revisionArgs = toInputArgs(revisionSchema);
-  const labelArgs = toInputArgs(labelSchema);
-  const issueLabelArgs = toInputArgs(issueLabelSchema);
-  return {
-    user: new MemorySource('user', userArgs.columns, userArgs.primaryKey),
-    issue: new MemorySource('issue', issueArgs.columns, issueArgs.primaryKey),
-    comment: new MemorySource(
-      'comment',
-      commentArgs.columns,
-      commentArgs.primaryKey,
-    ),
-    revision: new MemorySource(
-      'revision',
-      revisionArgs.columns,
-      revisionArgs.primaryKey,
-    ),
-    label: new MemorySource('label', labelArgs.columns, labelArgs.primaryKey),
-    issueLabel: new MemorySource(
-      'issueLabel',
-      issueLabelArgs.columns,
-      issueLabelArgs.primaryKey,
-    ),
-  };
-}
+function addData(queryDelegate: QueryDelegate) {
+  queryDelegate
+    .getSource('user')
+    .push({type: 'add', row: {id: '001', name: 'Alice'}});
+  queryDelegate
+    .getSource('user')
+    .push({type: 'add', row: {id: '002', name: 'Bob'}});
+  queryDelegate
+    .getSource('user')
+    .push({type: 'add', row: {id: '003', name: 'Charlie'}});
+  queryDelegate
+    .getSource('user')
+    .push({type: 'add', row: {id: '004', name: 'Daniel'}});
 
-function makeHost(): Host & SubscriptionDelegate {
-  const sources = makeSources();
-  return {
-    getSource(tableName: string) {
-      return must(sources[tableName as keyof typeof sources]);
-    },
-    createStorage() {
-      return new MemoryStorage();
-    },
-    subscriptionAdded() {
-      return () => {};
-    },
-  };
-}
-
-function addData(host: Host) {
-  host.getSource('user').push({type: 'add', row: {id: '001', name: 'Alice'}});
-  host.getSource('user').push({type: 'add', row: {id: '002', name: 'Bob'}});
-  host.getSource('user').push({type: 'add', row: {id: '003', name: 'Charlie'}});
-  host.getSource('user').push({type: 'add', row: {id: '004', name: 'Daniel'}});
-
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '101',
@@ -75,7 +27,7 @@ function addData(host: Host) {
       ownerId: '001',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '102',
@@ -85,7 +37,7 @@ function addData(host: Host) {
       ownerId: '001',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '103',
@@ -96,7 +48,7 @@ function addData(host: Host) {
     },
   });
 
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '104',
@@ -106,7 +58,7 @@ function addData(host: Host) {
       ownerId: '002',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '105',
@@ -116,7 +68,7 @@ function addData(host: Host) {
       ownerId: '002',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '106',
@@ -126,7 +78,7 @@ function addData(host: Host) {
       ownerId: '002',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '107',
@@ -136,7 +88,7 @@ function addData(host: Host) {
       ownerId: '003',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '108',
@@ -146,7 +98,7 @@ function addData(host: Host) {
       ownerId: '003',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '109',
@@ -156,7 +108,7 @@ function addData(host: Host) {
       ownerId: '003',
     },
   });
-  host.getSource('issue').push({
+  queryDelegate.getSource('issue').push({
     type: 'add',
     row: {
       id: '110',
@@ -167,7 +119,7 @@ function addData(host: Host) {
     },
   });
 
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '201',
@@ -177,7 +129,7 @@ function addData(host: Host) {
       createdAt: 1,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '202',
@@ -187,7 +139,7 @@ function addData(host: Host) {
       createdAt: 2,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '203',
@@ -197,7 +149,7 @@ function addData(host: Host) {
       createdAt: 3,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '204',
@@ -207,7 +159,7 @@ function addData(host: Host) {
       createdAt: 4,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '205',
@@ -217,7 +169,7 @@ function addData(host: Host) {
       createdAt: 5,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '206',
@@ -227,7 +179,7 @@ function addData(host: Host) {
       createdAt: 6,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '207',
@@ -237,7 +189,7 @@ function addData(host: Host) {
       createdAt: 7,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '208',
@@ -247,7 +199,7 @@ function addData(host: Host) {
       createdAt: 8,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '209',
@@ -257,7 +209,7 @@ function addData(host: Host) {
       createdAt: 9,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '210',
@@ -267,7 +219,7 @@ function addData(host: Host) {
       createdAt: 10,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '211',
@@ -277,7 +229,7 @@ function addData(host: Host) {
       createdAt: 11,
     },
   });
-  host.getSource('comment').push({
+  queryDelegate.getSource('comment').push({
     type: 'add',
     row: {
       id: '212',
@@ -288,65 +240,66 @@ function addData(host: Host) {
     },
   });
 
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '301', commentId: '209', text: 'Revision 1', authorId: '001'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '302', commentId: '209', text: 'Revision 2', authorId: '001'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '303', commentId: '209', text: 'Revision 3', authorId: '001'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '304', commentId: '208', text: 'Revision 1', authorId: '002'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '305', commentId: '208', text: 'Revision 2', authorId: '002'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '306', commentId: '208', text: 'Revision 3', authorId: '002'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '307', commentId: '211', text: 'Revision 1', authorId: '003'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '308', commentId: '211', text: 'Revision 2', authorId: '003'},
   });
-  host.getSource('revision').push({
+  queryDelegate.getSource('revision').push({
     type: 'add',
     row: {id: '309', commentId: '211', text: 'Revision 3', authorId: '003'},
   });
 
-  host.getSource('label').push({type: 'add', row: {id: '401', name: 'bug'}});
-  host
+  queryDelegate
+    .getSource('label')
+    .push({type: 'add', row: {id: '401', name: 'bug'}});
+  queryDelegate
     .getSource('label')
     .push({type: 'add', row: {id: '402', name: 'feature'}});
 
-  host
+  queryDelegate
     .getSource('issueLabel')
     .push({type: 'add', row: {issueId: '103', labelId: '401'}});
-  host
+  queryDelegate
     .getSource('issueLabel')
     .push({type: 'add', row: {issueId: '102', labelId: '401'}});
-  host
+  queryDelegate
     .getSource('issueLabel')
     .push({type: 'add', row: {issueId: '102', labelId: '402'}});
 }
 
 describe('kitchen sink query', () => {
   test('complex query with filters, limits, and multiple joins', () => {
-    const host = makeHost();
-    addData(host);
-
-    const issueQuery = newQuery(host, issueSchema)
+    const queryDelegate = new QueryDelegateImpl();
+    addData(queryDelegate);
+    const issueQuery = newQuery(queryDelegate, issueSchema)
       .where('ownerId', 'IN', ['001', '002', '003'])
       .where('closed', '=', false)
       .related('owner', q => q.select('name'))
@@ -403,24 +356,12 @@ describe('kitchen sink query', () => {
         id: '102',
         labels: [
           {
-            issueId: '102',
-            labelId: '401',
-            labels: [
-              {
-                id: '401',
-                name: 'bug',
-              },
-            ],
+            id: '401',
+            name: 'bug',
           },
           {
-            issueId: '102',
-            labelId: '402',
-            labels: [
-              {
-                id: '402',
-                name: 'feature',
-              },
-            ],
+            id: '402',
+            name: 'feature',
           },
         ],
         owner: [
@@ -470,14 +411,8 @@ describe('kitchen sink query', () => {
         id: '103',
         labels: [
           {
-            issueId: '103',
-            labelId: '401',
-            labels: [
-              {
-                id: '401',
-                name: 'bug',
-              },
-            ],
+            id: '401',
+            name: 'bug',
           },
         ],
         owner: [
