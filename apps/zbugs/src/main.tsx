d3f86bb8e4a0898efea90fe87d64e7f7a830e591
Aaron Boodman <aaron@aaronboodman.com>
2024-09-04 00:08:28 -1000

    chore(zbugs): First bits of Zero.
    Server still doesn't start for some reason.

--- apps/zbugs/src/main.tsx
+++ apps/zbugs/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import {z} from './zero.ts';
+
+preload();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+function preload() {
+  const sorts = ['modified', 'created', 'priority', 'status'] as const;
+  for (const sort of sorts) {
+    z.query.issue
+      .related('labels')
+      .related('comments', comments => comments.limit(20))
+      .orderBy(sort, 'desc')
+      .limit(1_000)
+      .preload();
+  }
+}
--- apps/zbugs/vite.config.ts
+++ apps/zbugs/vite.config.ts
@@ -1,15 +1,22 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import {makeDefine} from 'shared/src/build.js';
 
 const ReactCompilerConfig = {};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: makeDefine(),
   plugins: [
+    tsconfigPaths(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
       },
     }),
   ],
+  build: {
+    target: 'esnext',
+  },
 });
