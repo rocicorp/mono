aad086fc82897f933663a07730b54ac43d5cf256
Erik Arvidsson <arv@roci.dev>
2024-09-16 11:10:44 +0200

    expand test

--- packages/zql/src/zql/ivm/take.push.test.ts
+++ packages/zql/src/zql/ivm/take.push.test.ts
@@ -1045,6 +1045,60 @@ suite('take with no partition', () => {
         ],
       });
     });
+
+    takeTest({
+      ...base,
+      name: 'at limit 1',
+      limit: 1,
+      pushes: [
+        {
+          type: 'edit',
+          oldRow: {id: 'i1', created: 100, text: 'a'},
+          row: {id: 'i1', created: 50, text: 'a2'},
+        },
+      ],
+      expectedMessages: [
+        [
+          'takeSnitch',
+          'push',
+          {
+            type: 'edit',
+            oldRow: {id: 'i1', created: 100, text: 'a'},
+            row: {id: 'i1', created: 50, text: 'a2'},
+          },
+        ],
+      ],
+      expectedStorage: {
+        '["take",null]': {
+          bound: {
+            created: 50,
+            id: 'i1',
+            text: 'a2',
+          },
+          size: 1,
+        },
+        'maxBound': {
+          created: 100,
+          id: 'i1',
+          text: 'a',
+        },
+      },
+      expectedOutput: [
+        {
+          oldRow: {
+            created: 100,
+            id: 'i1',
+            text: 'a',
+          },
+          row: {
+            created: 50,
+            id: 'i1',
+            text: 'a2',
+          },
+          type: 'edit',
+        },
+      ],
+    });
   });
 });
 
