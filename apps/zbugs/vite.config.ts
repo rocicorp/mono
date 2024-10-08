79c40f7463cd300988225107ca881479ac5b18d0
Erik Arvidsson <arv@roci.dev>
2024-10-08 12:12:51 +0200

    run it on zbugs

--- apps/zbugs/vite.config.ts
+++ apps/zbugs/vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import {makeDefine} from 'shared/src/build.js';
+import {makeDefine} from '../../packages/shared/src/build.js';
 import {defineConfig, type ViteDevServer} from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
