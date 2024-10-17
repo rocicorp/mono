2c89c2d400f14d36440dc49aee128cea68b63654
Erik Arvidsson <arv@roci.dev>
2024-10-17 17:34:54 +0200

    use title prop

--- apps/zbugs/src/components/not-logged-in-modal.tsx
+++ apps/zbugs/src/components/not-logged-in-modal.tsx
@@ -1,4 +1,3 @@
-import CloseIcon from '../assets/icons/close.svg?react';
 import Modal from './modal.js';
 
 export interface Props {
@@ -9,18 +8,8 @@ export interface Props {
 
 export function NotLoggedInModal({onDismiss, isOpen, href}: Props) {
   return (
-    <Modal isOpen={isOpen} onDismiss={onDismiss}>
+    <Modal isOpen={isOpen} onDismiss={onDismiss} title="Not Logged In">
       <div className="flex flex-col w-full py-4 overflow-hidden modal-container">
-        <div className="flex items-center justify-between flex-shrink-0 px-4 issue-composer-header">
-          <div className="flex items-center">
-            <span className="issue-detail-label">Not Logged In</span>
-          </div>
-          <div className="flex items-center">
-            <button className="inline-flex rounded items-center justify-center text-gray-500 h-7 w-7 rouned hover:text-gray-700">
-              <CloseIcon className="w-4" />
-            </button>
-          </div>
-        </div>
         <div className="flex flex-col flex-1 pb-3.5 overflow-y-auto">
           <div className="flex items-center w-full mt-1.5 px-4">
             <p>You need to be logged in to create a new issue.</p>
