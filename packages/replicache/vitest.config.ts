b7fedad337e3e6a94ffeeac409727a0731fcd601
Erik Arvidsson <arv@roci.dev>
2024-10-17 15:04:20 +0200

    chore(replicache): Remove --globals

--- packages/replicache/vitest.config.ts
+++ packages/replicache/vitest.config.ts
@@ -1,11 +1,1 @@
-import {defineConfig} from 'vitest/config';
-import {config} from '../shared/src/tool/vitest-config.js';
-
-export default defineConfig({
-  ...config,
-  test: {
-    ...config.test,
-    // TODO: Fix this
-    globals: true,
-  },
-});
+export {config as default} from '../shared/src/tool/vitest-config.js';
