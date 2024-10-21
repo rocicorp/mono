667969445a3665d295861469c25d77cbc852f548
Erik Arvidsson <arv@roci.dev>
2024-10-20 20:33:11 -0700

    more

--- apps/zbugs/src/pages/issue/comment-composer.tsx
+++ apps/zbugs/src/pages/issue/comment-composer.tsx
@@ -58,11 +58,7 @@ export default function CommentComposer({
         {id ? 'Save' : 'Add comment'}
       </button>{' '}
       {id ? (
-        <button
-          className="edit-comment-cancel"
-          onMouseDown={onDone}
-          disabled={currentBody.trim().length === 0}
-        >
+        <button className="edit-comment-cancel" onMouseDown={onDone}>
           Cancel
         </button>
       ) : null}
