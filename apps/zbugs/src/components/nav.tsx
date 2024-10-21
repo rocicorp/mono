cbb71d3bf5497ee7a0ea9ce906f53aa4f13a5af0
Erik Arvidsson <arv@roci.dev>
2024-10-21 09:48:47 -0700

    Revert "feat(zbugs): Remove FPS meter"
    This reverts commit fc1e44cace80dd32caf0f98e08a5555b771e8192.

--- apps/zbugs/src/components/nav.tsx
+++ apps/zbugs/src/components/nav.tsx
@@ -1,3 +1,4 @@
+import {FPSMeter} from '@schickling/fps-meter';
 import classNames from 'classnames';
 import {useState} from 'react';
 import {useSearch} from 'wouter';
@@ -8,9 +9,9 @@ import markURL from '../assets/images/mark.svg';
 import {useLogin} from '../hooks/use-login.js';
 import {useZero} from '../hooks/use-zero.js';
 import IssueComposer from '../pages/issue/issue-composer.js';
-import {links} from '../routes.js';
 import {Link} from './link.js';
 import {NotLoggedInModal} from './not-logged-in-modal.js';
+import {links} from '../routes.js';
 
 export function Nav() {
   const qs = new URLSearchParams(useSearch());
@@ -86,6 +87,7 @@ export function Nav() {
           </Link>
         </div>
 
+        <FPSMeter className="fps-meter" width={192} height={38} />
         <div className="user-login">
           {login.loginState === undefined ? (
             <a href={loginHref}>Login</a>
