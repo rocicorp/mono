cef69f27cdea59039b95ec5c2c1bc95f86172dd2
Darick Tong <d@roci.dev>
2024-10-22 15:56:19 -0700

    chore(zero-cache): consolidate validation into publication setup

---
+++ packages/zero-cache/src/services/view-syncer/schema/init.ts
@@ -0,0 +1,36 @@
+import type {LogContext} from '@rocicorp/logger';
+import type postgres from 'postgres';
+import {
+  runSchemaMigrations,
+  type IncrementalMigrationMap,
+  type Migration,
+} from '../../../db/migration.js';
+import {PG_SCHEMA, setupCVRTables} from './cvr.js';
+
+const setupMigration: Migration = {
+  migrateSchema: setupCVRTables,
+  minSafeVersion: 1,
+};
+
+export async function initViewSyncerSchema(
+  log: LogContext,
+  db: postgres.Sql,
+): Promise<void> {
+  const schemaVersionMigrationMap: IncrementalMigrationMap = {
+    1: setupMigration,
+    // There are no incremental migrations yet, but if we were to, say introduce
+    // another column, setupCDCTables would be updated to create the table with
+    // the new column, and then there would be an incremental migration here at
+    // version `2` that adds the column for databases that were initialized to
+    // version `1`.
+  };
+
+  await runSchemaMigrations(
+    log,
+    'view-syncer',
+    PG_SCHEMA,
+    db,
+    setupMigration,
+    schemaVersionMigrationMap,
+  );
+}
