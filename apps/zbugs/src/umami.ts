eebed2b749a2b1aeaad78c1a9072fa17e0b46423
Erik Arvidsson <arv@roci.dev>
2024-11-12 21:17:58 +0100

    Fix type

--- apps/zbugs/src/umami.ts
+++ apps/zbugs/src/umami.ts
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 // Based on
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/4677b2523d7cdb6545907896b7dcaba098f22ce1/types/umami/index.d.ts
 // But we do not want to add @types/umami because browsers block tracking
 // scripts so the global might be undefined
 
 interface Umami {
-  track(eventName: string): void;
+  track(eventName: string, eventData?: {[key: string]: any}): void;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const umami: Umami = (globalThis as any).umami ?? {
   track() {
     // no op
