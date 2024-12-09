2987652d4996351a53fda8e43a5d971babd3d2fd
Cesar Alaestante <cesar@roci.dev>
2024-12-09 11:33:42 -0700

    fix launch script

--- packages/zero-cache/start-dual.sh
+++ packages/zero-cache/start-dual.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-# Define the path to the database file
-DB_PATH="/data/db/sync-replica.db"
-
 # Check if the database file exists
-if [ ! -f "$DB_PATH" ]; then
+if [ ! -f "$ZERO_REPLICA_FILE" ]; then
   echo "Database file does not exist. Restoring from backup..."
-  litestream restore -if-db-not-exists -if-replica-exists -o "$DB_PATH" "$REPLICA_URL"
+  litestream restore -if-db-not-exists -if-replica-exists -o "$ZERO_REPLICA_FILE" "$REPLICA_URL"
 else
   echo "Database file already exists. Skipping restore."
 fi
 
 if [ "$ZERO_LITESTREAM" = "1" ] || [ "$ZERO_LITESTREAM" = "true" ]; then
-  litestream replicate -config /opt/app/prod/sandbox/replication-manager.yml
+  echo "Launching Litestream"
+  litestream replicate -config prod/zbugs/litestream.yml
 else 
-npx tsx ./src/server/main.ts
+  echo "Not Launching Litestream and running main"
+  npx tsx ./src/server/main.ts
 fi
 
 
