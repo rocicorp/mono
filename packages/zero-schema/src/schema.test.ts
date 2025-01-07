184c11dd5efe083f106a1283709fdfd81649d051
jer-k <jeremyk112@gmail.com>
2025-01-07 09:50:20 -0800

    Add assertion for all relationship tables in createSchema

--- packages/zero-schema/src/schema.test.ts
+++ packages/zero-schema/src/schema.test.ts
@@ -25,3 +25,101 @@ test('Unexpected tableName should throw', () => {
     'createSchema tableName mismatch, expected bar === bars',
   );
 });
+
+test('Missing table in direct relationship should throw', () => {
+  const bar = {
+    tableName: 'bar',
+    primaryKey: ['id'],
+    columns: {
+      id: {type: 'number'},
+    },
+  } as const;
+
+  const foo = {
+    tableName: 'foo',
+    primaryKey: ['id'],
+    columns: {
+      id: {type: 'number'},
+      bar_id: {type: 'number'},
+    },
+    relationships: {
+      bar: {
+        sourceField: 'bar_id',
+        destSchema: () => bar,
+        destField: 'id',
+      },
+    },
+  } as const;
+
+  const schema = {
+    version: 1,
+    tables: {
+      foo: foo,
+    },
+  } as const;
+
+  expect(() => createSchema(schema)).toThrow(
+    'createSchema relationship missing, foo relationship bar not present in schema.tables',
+  );
+});
+
+test('Missing table in junction relationship should throw', () => {
+  const baz = {
+    tableName: 'baz',
+    primaryKey: ['id'],
+    columns: {
+      id: {type: 'number'},
+    },
+  } as const;
+
+  const bar = {
+    tableName: 'bar',
+    primaryKey: ['id'],
+    columns: {
+      id: {type: 'number'},
+      baz_id: {type: 'number'},
+    },
+    relationships: {
+      baz: {
+        sourceField: 'baz_id',
+        destSchema: () => baz,
+        destField: 'id',
+      },
+    },
+  } as const;
+
+  const foo = {
+    tableName: 'foo',
+    primaryKey: ['id'],
+    columns: {
+      id: {type: 'number'},
+      bar_id: {type: 'number'},
+    },
+    relationships: {
+      baz: [
+        {
+          sourceField: 'bar_id',
+          destSchema: () => bar,
+          destField: 'id',
+        },
+        {
+          sourceField: 'baz_id',
+          destSchema: () => baz,
+          destField: 'id',
+        },
+      ],
+    },
+  } as const;
+
+  const schema = {
+    version: 1,
+    tables: {
+      foo: foo,
+      bar: bar,
+    },
+  } as const;
+
+  expect(() => createSchema(schema)).toThrow(
+    'createSchema relationship missing, foo relationship baz not present in schema.tables',
+  );
+});
--- packages/zero-schema/src/schema.ts
+++ packages/zero-schema/src/schema.ts
@@ -1,9 +1,14 @@
 import {assert} from '../../shared/src/asserts.js';
-import type {TableSchema} from './table-schema.js';
+import {
+  type FieldRelationship,
+  type Relationship,
+  type TableSchema,
+  isFieldRelationship,
+} from './table-schema.js';
 
 export type Schema = {
   readonly version: number;
-  readonly tables: {readonly [table: string]: TableSchema};
+  readonly tables: { readonly [table: string]: TableSchema };
 };
 
 export function createSchema<const S extends Schema>(schema: S): S {
@@ -12,6 +17,67 @@ export function createSchema<const S extends Schema>(schema: S): S {
       tableName === table.tableName,
       `createSchema tableName mismatch, expected ${tableName} === ${table.tableName}`,
     );
+
+    // Assert that all relationship -> destSchema entries are present in schema.tables
+    const { relationships } = table;
+    if (relationships) {
+      for (const [
+        relationshipName,
+        relationship,
+      ] of Object.entries(relationships)) {
+        checkRelationship(
+          relationship,
+          tableName,
+          relationshipName as keyof S['tables'][typeof tableName]['relationships'],
+          schema,
+        );
+      }
+    }
   }
   return schema as S;
+}
+
+function checkDestSchema<
+  S extends Schema,
+  TableName extends keyof S['tables'],
+  RelationshipName extends keyof S['tables'][TableName]['relationships'],
+>(
+  schema: S,
+  tableName: TableName,
+  relationshipName: RelationshipName,
+  relationship: FieldRelationship,
+) {
+  const destSchema =
+    typeof relationship.destSchema === 'function'
+      ? relationship.destSchema()
+      : relationship.destSchema;
+
+  assert(
+    schema.tables[destSchema.tableName],
+    `createSchema relationship missing, ${String(tableName)} relationship ${String(relationshipName)} not present in schema.tables`,
+  );
+}
+
+function checkRelationship<
+  const S extends Schema,
+  TableName extends keyof S['tables'],
+  RelationshipName extends keyof S['tables'][TableName]['relationships'],
+>(
+  relationship: Relationship,
+  tableName: TableName,
+  relationshipName: RelationshipName,
+  schema: S,
+) {
+  if (isFieldRelationship(relationship)) {
+    checkDestSchema(schema, tableName, relationshipName, relationship);
+  } else {
+    for (const junctionRelationship of relationship) {
+      checkDestSchema(
+        schema,
+        tableName,
+        relationshipName,
+        junctionRelationship,
+      );
+    }
+  }
 }
