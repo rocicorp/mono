0141258ba4ead92d86fc932d30d2d12f580e0120
Darick Tong <d@roci.dev>
2025-01-09 21:54:11 -0800

    feat(zero-cache): run/manage litestream from within zero-cache

---
+++ packages/zero-cache/src/services/litestream/commands.ts
@@ -0,0 +1,89 @@
+import type {LogContext} from '@rocicorp/logger';
+import {resolver} from '@rocicorp/resolver';
+import {ChildProcess, spawn} from 'node:child_process';
+import {existsSync} from 'node:fs';
+import {must} from '../../../../shared/src/must.js';
+import {sleep} from '../../../../shared/src/sleep.js';
+import type {ZeroConfig} from '../../config/zero-config.js';
+
+type ZeroLitestreamConfig = Pick<
+  ZeroConfig,
+  'log' | 'replicaFile' | 'litestream'
+>;
+
+export async function restoreReplica(
+  lc: LogContext,
+  config: ZeroLitestreamConfig,
+  maxRetries: number,
+  retryIntervalMs = 3000,
+) {
+  for (let i = 0; i < maxRetries; i++) {
+    if (i > 0) {
+      lc.info?.(
+        `replica not found. retrying in ${retryIntervalMs / 1000} seconds`,
+      );
+      await sleep(retryIntervalMs);
+    }
+    const restored = await tryRestore(config);
+    if (restored) {
+      return;
+    }
+    if (maxRetries === 1) {
+      lc.info?.('no litestream backup found');
+      return;
+    }
+  }
+  throw new Error(`max attempts exceeded restoring replica`);
+}
+
+function getLitestream(config: ZeroLitestreamConfig): {
+  litestream: string;
+  env: NodeJS.ProcessEnv;
+} {
+  const {executable, backupLocation, logLevel, configPath} = config.litestream;
+  return {
+    litestream: must(executable, `Missing --litestream-executable`),
+    env: {
+      ...process.env,
+      ['ZERO_REPLICA_FILE']: config.replicaFile,
+      ['ZERO_LITESTREAM_BACKUP_LOCATION']: must(backupLocation),
+      ['ZERO_LITESTREAM_LOG_LEVEL']: logLevel,
+      ['ZERO_LOG_FORMAT']: config.log.format,
+      ['LITESTREAM_CONFIG']: configPath,
+    },
+  };
+}
+
+async function tryRestore(config: ZeroLitestreamConfig) {
+  const {litestream, env} = getLitestream(config);
+  const proc = spawn(
+    litestream,
+    ['restore', '-if-db-not-exists', '-if-replica-exists', config.replicaFile],
+    {
+      env: {
+        ...env,
+        // The output for litestream restore is minimal. Include it all.
+        ['ZERO_LITESTREAM_LOG_LEVEL']: 'debug',
+      },
+      stdio: 'inherit',
+    },
+  );
+  const {promise, resolve, reject} = resolver();
+  proc.on('error', reject);
+  proc.on('close', (code, signal) => {
+    if (signal) {
+      reject(`litestream killed with ${signal}`);
+    } else if (code !== 0) {
+      reject(`litestream exited with code ${code}`);
+    } else {
+      resolve();
+    }
+  });
+  await promise;
+  return existsSync(config.replicaFile);
+}
+
+export function backupReplica(config: ZeroLitestreamConfig): ChildProcess {
+  const {litestream, env} = getLitestream(config);
+  return spawn(litestream, ['replicate'], {env, stdio: 'inherit'});
+}
