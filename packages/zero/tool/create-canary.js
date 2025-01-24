6fe1e780103827c31166271b526740cd4b53b808
Cesar Alaestante <cesar@roci.dev>
2025-01-24 11:42:31 -0700

    chore(infra): remove unecessary token

--- packages/zero/tool/create-canary.js
+++ packages/zero/tool/create-canary.js
@@ -103,13 +103,6 @@ function bumpCanaryVersion(version, hash) {
 const buildBranch = process.argv[2] ?? 'main';
 console.log(`Releasing from branch: ${buildBranch}`);
 
-const npmAuthToken = execute("cat $HOME/.npmrc | grep '_authToken'", {
-  stdio: 'pipe',
-});
-if (!npmAuthToken) {
-  console.error('No npm auth token found in ~/.npmrc');
-  process.exit(1);
-}
 
 try {
   const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'zero-build-'));
@@ -203,7 +196,6 @@ try {
         `docker buildx build \
     --platform linux/amd64,linux/arm64 \
     --build-arg=ZERO_VERSION=${nextCanaryVersion} \
-    --build-arg=NPM_TOKEN=${npmAuthToken} \
     -t rocicorp/zero:${dockerCanaryVersion} \
     -t rocicorp/zero:canary \
     --push .`,
