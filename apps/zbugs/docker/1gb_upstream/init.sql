96beb73cb1e3b079ec07e4b509ed3c524ca08091
Cesar Alaestante <cesar@roci.dev>
2025-02-06 15:57:18 -0700

    chore(infra): 1gb of data

--- apps/zbugs/docker/1gb_upstream/init.sql
+++ apps/zbugs/docker/1gb_upstream/init.sql
@@ -277,23 +277,83 @@ VALUES (true, 3, 5) ON CONFLICT DO NOTHING;
 
 COPY "user"
 FROM
-    '/docker-entrypoint-initdb.d/users.csv' WITH CSV HEADER;
+    '/data/users.csv' WITH CSV HEADER;
 
 COPY "label"
 FROM
-    '/docker-entrypoint-initdb.d/labels.csv' WITH CSV HEADER;
+    '/data/labels.csv' WITH CSV HEADER;
 
-COPY "issue" ("id","shortID","title","open","modified","created","creatorID","assigneeID","description")
+COPY "issue"
 FROM
-    '/docker-entrypoint-initdb.d/issues.csv' WITH CSV HEADER;
+    '/data/issues.csv' WITH CSV HEADER;
+
+COPY "issue"
+FROM
+    '/data/issues_2.csv' WITH CSV HEADER;
+
+COPY "issue"
+FROM
+    '/data/issues_3.csv' WITH CSV HEADER;
+
+COPY "issue"
+FROM
+    '/data/issues_4.csv' WITH CSV HEADER;
+
+COPY "issue"
+FROM
+    '/data/issues_5.csv' WITH CSV HEADER;
+
+COPY "issue"
+FROM
+    '/data/issues_6.csv' WITH CSV HEADER;
 
 COPY "issueLabel"
 FROM
-    '/docker-entrypoint-initdb.d/issue_labels.csv' WITH CSV HEADER;
+    '/data/issue_labels.csv' WITH CSV HEADER;
+
+COPY "issueLabel"
+FROM
+    '/data/issue_labels_2.csv' WITH CSV HEADER;
+
+COPY "issueLabel"
+FROM
+    '/data/issue_labels_3.csv' WITH CSV HEADER;
+
+COPY "issueLabel"
+FROM
+    '/data/issue_labels_4.csv' WITH CSV HEADER;
+
+COPY "issueLabel"
+FROM
+    '/data/issue_labels_5.csv' WITH CSV HEADER;
+
+COPY "issueLabel"
+FROM
+    '/data/issue_labels_6.csv' WITH CSV HEADER;
 
 COPY "comment"
 FROM
-    '/docker-entrypoint-initdb.d/comments.csv' WITH CSV HEADER;
+    '/data/comments.csv' WITH CSV HEADER;
+
+COPY "comment"
+FROM
+    '/data/comments_2.csv' WITH CSV HEADER;
+
+COPY "comment"
+FROM
+    '/data/comments_3.csv' WITH CSV HEADER;
+
+COPY "comment"
+FROM
+    '/data/comments_4.csv' WITH CSV HEADER;
+
+COPY "comment"
+FROM
+    '/data/comments_5.csv' WITH CSV HEADER;
+
+COPY "comment"
+FROM
+    '/data/comments_6.csv' WITH CSV HEADER;
 
 
 -- Create the indices on upstream so we can copy to downstream on replication.
