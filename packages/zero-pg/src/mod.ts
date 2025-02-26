b9414666670d50bee5bd5718ea9cd59efa20b732
Matt Wonlaw <mlaw@roci.dev>
2025-02-26 16:39:51 -0500

    feat(zero-pg): add the zero-pg package to `zero`

---
+++ packages/zero-pg/src/mod.ts
@@ -0,0 +1,10 @@
+export {createPushHandler} from './custom.ts';
+export type {CustomMutatorDefs, CustomMutatorImpl} from './custom.ts';
+export type {
+  Transaction,
+  ServerTransaction,
+  DBConnection,
+  DBTransaction,
+  ConnectionProvider,
+  Row,
+} from '../../zql/src/mutate/custom.ts';
