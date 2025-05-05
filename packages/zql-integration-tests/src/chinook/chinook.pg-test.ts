a64c3499a021c45f009ce9b7b8f2af00de931122
Matt Wonlaw <mlaw@roci.dev>
2025-05-05 11:11:17 -0400

    fix(zql): O(logn) point queries
    No more full table scans to do a point query by primary key on the client.

--- packages/zql-integration-tests/src/chinook/chinook.pg-test.ts
+++ packages/zql-integration-tests/src/chinook/chinook.pg-test.ts
@@ -26,7 +26,6 @@ test.each(
     {
       suiteName: 'compiler_chinook',
       pgContent,
-      only: 'In operator',
       zqlSchema: schema,
       setRawData: r => {
         data = r;
@@ -174,6 +173,22 @@ test.each(
             .limit(10),
       },
     ],
+    // primary key compare for each table
+    (() =>
+      tables.map(
+        table =>
+          ({
+            name: `${table} pk lookup`,
+            createQuery: q => {
+              const pk = schema.tables[table].primaryKey;
+              let ret = q[table] as AnyQuery;
+              for (const column of pk) {
+                ret = ret.where(column, '=', 1);
+              }
+              return ret;
+            },
+          }) as const,
+      ))(),
     // compare against primary key for each operator
     (() =>
       operators.map(
