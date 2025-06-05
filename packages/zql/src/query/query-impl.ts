079a12f8a0d6f10cf02760fdf96b7bce682fdf6d
Matt Wonlaw <mlaw@roci.dev>
2025-06-05 10:30:13 -0400

    chore(zql): allow static queries to be converted to runnable queries
    This is required for the custom query API.
    
    Custom queries are defined without reference to a Zero instance. They are bound to a query delegate at runtime.

--- packages/zql/src/query/query-impl.ts
+++ packages/zql/src/query/query-impl.ts
@@ -636,7 +636,7 @@ export class QueryImpl<
   TTable extends keyof TSchema['tables'] & string,
   TReturn = PullRow<TTable, TSchema>,
 > extends AbstractQuery<TSchema, TTable, TReturn> {
-  readonly #delegate: QueryDelegate;
+  #delegate: QueryDelegate;
   readonly #system: System;
 
   constructor(
@@ -679,6 +679,7 @@ export class QueryImpl<
     );
   }
 
+  materialize(): TypedView<HumanReadable<TReturn>>;
   materialize<T>(
     factoryOrTTL?: ViewFactory<TSchema, TTable, TReturn, T> | TTL,
     ttl: TTL = DEFAULT_TTL,
--- packages/zql/src/query/static-query.ts
+++ packages/zql/src/query/static-query.ts
@@ -2,7 +2,13 @@ import type {AST} from '../../../zero-protocol/src/ast.ts';
 import type {Schema} from '../../../zero-schema/src/builder/schema-builder.ts';
 import type {Format} from '../ivm/view.ts';
 import {ExpressionBuilder} from './expression.ts';
-import {AbstractQuery, defaultFormat, newQuerySymbol} from './query-impl.ts';
+import {
+  AbstractQuery,
+  defaultFormat,
+  newQuerySymbol,
+  QueryImpl,
+  type QueryDelegate,
+} from './query-impl.ts';
 import type {HumanReadable, PullRow, Query} from './query.ts';
 import type {TypedView} from './typed-view.ts';
 
@@ -27,9 +33,11 @@ export class StaticQuery<
   TTable extends keyof TSchema['tables'] & string,
   TReturn = PullRow<TTable, TSchema>,
 > extends AbstractQuery<TSchema, TTable, TReturn> {
-  expressionBuilder() {
-    return new ExpressionBuilder(this._exists);
-  }
+  readonly #schema: TSchema;
+  readonly #tableName: TTable;
+  readonly #ast: AST;
+  readonly #format: Format;
+  readonly #currentJunction?: string | undefined;
 
   constructor(
     schema: TSchema,
@@ -39,6 +47,11 @@ export class StaticQuery<
     currentJunction?: string | undefined,
   ) {
     super(schema, tableName, ast, format, 'permissions', currentJunction);
+    this.#schema = schema;
+    this.#tableName = tableName;
+    this.#ast = ast;
+    this.#format = format;
+    this.#currentJunction = currentJunction;
   }
 
   protected [newQuerySymbol]<
@@ -57,6 +70,24 @@ export class StaticQuery<
 
   get ast() {
     return this._completeAst();
+  }
+
+  expressionBuilder() {
+    return new ExpressionBuilder(this._exists);
+  }
+
+  asRunnableQuery(
+    delegate: QueryDelegate,
+  ): QueryImpl<TSchema, TTable, TReturn> {
+    return new QueryImpl(
+      delegate,
+      this.#schema,
+      this.#tableName,
+      this.#ast,
+      this.#format,
+      'client',
+      this.#currentJunction,
+    );
   }
 
   materialize(): TypedView<HumanReadable<TReturn>> {
