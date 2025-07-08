8e1deff9c3d3a414afbe40a54eb3318258efe367
Darick Tong <d@roci.dev>
2025-07-08 10:46:34 -0700

    readability

--- packages/zero-cache/src/services/litestream/commands.ts
+++ packages/zero-cache/src/services/litestream/commands.ts
@@ -167,18 +167,14 @@ async function reserveAndGetSnapshotLocation(
   const {promise: backupURL, resolve, reject} = resolver<string>();
   try {
     assertNormalized(config);
-    const {
-      taskID,
-      change,
-      changeStreamer: {uri},
-    } = config;
+    const {taskID, change, changeStreamer} = config;
     const shardID = getShardConfig(config);
 
     const changeStreamerClient = new ChangeStreamerHttpClient(
       lc,
       shardID,
       change.db,
-      uri,
+      changeStreamer.uri,
     );
 
     const sub = await changeStreamerClient.reserveSnapshot(taskID);
