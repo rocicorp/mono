82091b68a075ea9a76fae7233273d9bc9a536afb
Erik Müller <erik@circonomit.de>
2025-07-12 00:01:15 +0200

    fix: format

--- packages/zero-solid/src/mod.ts
+++ packages/zero-solid/src/mod.ts
@@ -1,12 +1,7 @@
 export {
-	type CreateQueryOptions,
-	createQuery,
-	type UseQueryOptions,
-	useQuery,
-} from "./use-query.ts";
-export {
-	createUseZero,
-	createZero,
-	useZero,
-	ZeroProvider,
-} from "./use-zero.ts";
+  type CreateQueryOptions,
+  createQuery,
+  type UseQueryOptions,
+  useQuery,
+} from './use-query.ts';
+export {createUseZero, createZero, useZero, ZeroProvider} from './use-zero.ts';
