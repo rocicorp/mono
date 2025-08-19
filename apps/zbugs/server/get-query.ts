2a2f4c0aceb67455f28ba281e1cbf0ebc3441e2d
Gregory Baker <greg@roci.dev>
2025-08-18 21:37:17 -0700

    optimize queries server side in get-query
    refactor
    
    fixup
    
    type fix
    
    fix mutators
    
    triggers
    
    data update

--- apps/zbugs/server/get-query.ts
+++ apps/zbugs/server/get-query.ts
@@ -1,6 +1,21 @@
-import {withValidation, type ReadonlyJSONValue} from '@rocicorp/zero';
-import {queries} from '../shared/queries.ts';
+import {
+  withValidation,
+  type AnyQuery,
+  type Query,
+  type ReadonlyJSONValue,
+  type Row,
+  type Schema,
+} from '@rocicorp/zero';
+import {
+  buildBaseListQuery,
+  buildBaseListQueryFilter,
+  buildListQuery,
+  queries,
+  type ListContext,
+  type ListQueryArgs,
+} from '../shared/queries.ts';
 import type {AuthData} from '../shared/auth.ts';
+import {builder} from '../shared/schema.ts';
 
 // It's important to map incoming queries by queryName, not the
 // field name in queries. The latter is just a local identifier.
@@ -16,8 +31,129 @@ export function getQuery(
   args: readonly ReadonlyJSONValue[],
 ) {
   if (name in validated) {
+    if (name === 'issueList') {
+      //const [listContext, userID, limit] = queries.issueList.parse(args);
+      const [listContext, userID, limit] = args as [
+        ListContext['params'],
+        string,
+        number,
+      ];
+      return serverOptimizedListQuery(
+        {
+          listContext,
+          limit,
+          userID,
+          role: context?.role,
+        },
+        buildListQuery,
+      );
+    }
+    if (name === 'prevNext') {
+      const [listContext, issue, dir] = args as [
+        ListContext['params'] | null,
+        Pick<
+          Row<Schema['tables']['issue']>,
+          'id' | 'created' | 'modified'
+        > | null,
+        'next' | 'prev',
+      ];
+      return serverOptimizedListQuery(
+        {
+          listContext: listContext ?? undefined,
+          limit: 1,
+          start: issue ?? undefined,
+          dir,
+          role: context?.role,
+        },
+        buildBaseListQuery,
+      );
+    }
+
     return validated[name](context, ...args);
+  } else {
+    throw new Error(`Unknown query: ${name}`);
   }
 
   throw new Error(`Unknown query: ${name}`);
+}
+
+function serverOptimizedListQuery(
+  args: ListQueryArgs,
+  makeListQuery: (args: ListQueryArgs) => Query<Schema, 'issue'>,
+) {
+  const {listContext} = args;
+  if (!listContext) {
+    return builder.issue.where(({or}) => or());
+  }
+  const {assignee} = listContext;
+  if (assignee !== null && assignee !== undefined) {
+    return builder.user
+      .where('login', assignee)
+      .related('assignedIssues', q =>
+        makeListQuery({
+          ...args,
+          issueQuery: q,
+          listContext: {...listContext, assignee: null},
+        }),
+      )
+      .one();
+  }
+
+  const {creator} = listContext;
+  if (creator !== null && creator !== undefined) {
+    return builder.user
+      .where('login', creator)
+      .related('createdIssues', q =>
+        makeListQuery({
+          ...args,
+          issueQuery: q,
+          listContext: {...listContext, creator: null},
+        }),
+      )
+      .one();
+  }
+  const {labels} = listContext;
+  if (labels !== null && labels !== undefined && labels.length > 0) {
+    const [_, ...restLabels] = labels ?? [];
+    const restListContext = {...listContext, labels: restLabels};
+    return builder.label
+      .where('name', labels[0])
+      .related('issueLabels', q => {
+        q = q
+          .limit(args.limit)
+          .whereExists('issues', q =>
+            buildBaseListQueryFilter(q, restListContext, args.role),
+          )
+          .related('issues', q =>
+            makeListQuery({
+              ...args,
+              issueQuery: q,
+              listContext: restListContext,
+              limit: 1,
+            }),
+          );
+
+        const {sortDirection} = listContext;
+        const orderByDir =
+          args.dir === 'next'
+            ? sortDirection
+            : sortDirection === 'asc'
+              ? 'desc'
+              : 'asc';
+        q = q
+          .orderBy(
+            listContext.sortField === 'modified'
+              ? 'issueModified'
+              : 'issueCreated',
+            orderByDir,
+          )
+          .orderBy('issueID', orderByDir);
+        if (args.start) {
+          q = q.start(args.start);
+        }
+        return q;
+      })
+      .one();
+  }
+  return makeListQuery(args);
 }
