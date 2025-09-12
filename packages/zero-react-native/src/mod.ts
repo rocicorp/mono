06da0347fc9c2a3e73caafc9413def057319ee20
Erik Arvidsson <arv@roci.dev>
2025-09-12 11:05:53 +0200

    Also export opSQLiteStoreProvider for zero

--- packages/zero-react-native/src/mod.ts
+++ packages/zero-react-native/src/mod.ts
@@ -2,3 +2,7 @@ export {
   expoSQLiteStoreProvider,
   type ExpoSQLiteStoreOptions,
 } from '../../replicache/src/kv/expo-sqlite/store.ts';
+export {
+  opSQLiteStoreProvider,
+  type OpSQLiteStoreOptions,
+} from '../../replicache/src/kv/op-sqlite/store.ts';
