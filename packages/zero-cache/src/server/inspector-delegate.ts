f58f6181405408bbeeb6f86160fa414d6427a333
Erik Arvidsson <arv@roci.dev>
2025-09-13 10:11:25 +0200

    chore(zero): Fix inspector password in dev mode (#4892)
    When in development mode, we should not require an admin password. This
    changes the inspector handler to not send auth challenges when in dev
    mode.

--- packages/zero-cache/src/server/inspector-delegate.ts
+++ packages/zero-cache/src/server/inspector-delegate.ts
@@ -9,6 +9,7 @@ import {
   type MetricMap,
   type MetricsDelegate,
 } from '../../../zql/src/query/metrics-delegate.ts';
+import {isDevelopmentMode} from '../config/normalize.ts';
 
 /**
  * Server-side metrics collected for queries during materialization and update.
@@ -98,6 +99,9 @@ export class InspectorDelegate implements MetricsDelegate {
    * per "worker".
    */
   isAuthenticated(clientGroupID: ClientGroupID): boolean {
+    if (isDevelopmentMode()) {
+      return true;
+    }
     return authenticatedClientIDs.has(clientGroupID);
   }
 
