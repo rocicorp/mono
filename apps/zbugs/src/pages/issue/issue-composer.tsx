60d94ac2e56a7984600299ae8cf62489c152e422
Alexander King <alexhking@gmail.com>
2025-09-19 11:55:22 -0700

    Edits to Discord message on issue composer

--- apps/zbugs/src/pages/issue/issue-composer.tsx
+++ apps/zbugs/src/pages/issue/issue-composer.tsx
@@ -130,11 +130,10 @@ export function IssueComposer({isOpen, onDismiss}: Props) {
         </div>
         <div className="w-full px-4 mt-4">
           <p className="aside">
-            Testing zero? Please make sure to delete your comment after.
-            <br />
-            Want a faster response? Join us on Discord.
-            <a href="https://discord.rocicorp.dev/" className="aside-cta">
-              Discord
+            Testing Zero? Please make sure to delete your issue after. Want a
+            faster response?{' '}
+            <a href="https://discord.rocicorp.dev/">
+              Join us on Discord &rarr;
             </a>
           </p>
         </div>
