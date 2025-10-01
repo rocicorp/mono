8d5e357f29fa4acab16a292be1c9526f54ceb22e
Gregory Baker <greg@roci.dev>
2025-09-30 20:22:12 -0700

    publiccation

--- apps/zbugs/db/migrations/0001_add_project.sql
+++ apps/zbugs/db/migrations/0001_add_project.sql
@@ -2,9 +2,9 @@ CREATE TABLE "project" (
 	"id" varchar PRIMARY KEY NOT NULL,
 	"name" varchar NOT NULL
 );
---> BEGIN manual modification
+--> BEGIN manual modification for inserting Zero project
 INSERT INTO "project" ("id", "name") VALUES ('iCNlS2qEpzYWEes1RTf-D', 'Zero');
---> END manual modification
+--> END manual modification for inserting Zero project
 --> statement-breakpoint
 ALTER TABLE "issueLabel" DROP CONSTRAINT "issueLabel_labelID_fkey";
 --> statement-breakpoint
@@ -20,3 +20,7 @@ ALTER TABLE "issue" ADD CONSTRAINT "issue_projectID_fkey" FOREIGN KEY ("projectI
 ALTER TABLE "issueLabel" ADD CONSTRAINT "issueLabel_labelID_projectID_fkey" FOREIGN KEY ("labelID","projectID") REFERENCES "public"."label"("id","projectID") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "issueLabel" ADD CONSTRAINT "issueLabel_issueID_projectID_fkey" FOREIGN KEY ("issueID","projectID") REFERENCES "public"."issue"("id","projectID") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "label" ADD CONSTRAINT "label_projectID_fkey" FOREIGN KEY ("projectID") REFERENCES "public"."project"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+
+----> BEGIN manual modification for publication
+ALTER PUBLICATION zero_zbugs ADD TABLE project;
+----> END manual modification for publication
