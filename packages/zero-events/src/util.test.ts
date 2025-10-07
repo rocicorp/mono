0a2b81a5afc6083e40a6cb74024b99f84c6b7e8c
Darick Tong <d@roci.dev>
2025-10-06 17:01:26 -0700

    add test for Extend utility type

---
+++ packages/zero-events/src/util.test.ts
@@ -0,0 +1,21 @@
+import type {ZeroEvent} from './index.ts';
+import type {StatusEvent} from './status.ts';
+import type {Extend} from './util.ts';
+
+type IsValid<T> = [T] extends [never] ? false : true;
+
+export const extendAcceptsValidType: IsValid<
+  Extend<StatusEvent, {type: 'zero/events/status/foo/bar'}>
+> = true;
+
+export const extendRejectsInvalidType: IsValid<
+  Extend<StatusEvent, {type: 'not/a/proper/subtype'}>
+> = false;
+
+export const extendAllowsNarrowing: IsValid<
+  Extend<ZeroEvent, {type: 'foo/bar/baz'}>
+> = true;
+
+export const extendRejectsTypeChangeNarrowing: IsValid<
+  Extend<ZeroEvent, {type: number}>
+> = false;
