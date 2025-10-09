9839c6ab2113680e410d62d4bd6d0a1b11af8b16
Matt Wonlaw <mlaw@roci.dev>
2025-10-09 13:46:23 -0400

    commentary

--- packages/zql/src/planner/planner-connection.ts
+++ packages/zql/src/planner/planner-connection.ts
@@ -2,6 +2,55 @@ import type {Condition, Ordering} from '../../../zero-protocol/src/ast.ts';
 import type {Constraint} from '../ivm/constraint.ts';
 import type {PlannerNode} from './planner-node.ts';
 
+/**
+ * Represents a connection to a source.
+ *
+ * Connections have:
+ * - ordering
+ * - filters
+ * - constraints
+ *
+ * The ordering and filters are used to determine the initial cost of the connection.
+ *
+ * Once the planner has decided on connection to be the outer loop of the query plan,
+ * this will reveal constraints that can be sent to other connections.
+ *
+ * Example:
+ *
+ * ```ts
+ * builder.issue.whereExists('assignee', a => a.where('name', 'Alice'))
+ * ```
+ *
+ * ```
+ * [issue]  [assignee]
+ *   |         |
+ *   |         +-- where name = 'Alice'
+ *    \        /
+ *     \      /
+ *      [join]
+ *        |
+ * ```
+ *
+ * `issue` and `assignee` start with no constraints.
+ * Once the planner decides to make `issue` the outer loop, it will reveal
+ * a constraint on `assignee` of `issue.assignee_id = assignee.id`.
+ *
+ * If the planner decides to make `assignee` the outer loop, it will reveal
+ * a constraint on `issue` of `issue.assignee_id = assignee.id`.
+ *
+ * Constraints are propagated through the graph, sending them to their connections.
+ * Connections can update their cost based on the constraints they have received.
+ *
+ * The planner will query all connections for their cost and pick the lowest cost connection
+ * to be the next outer most loop.
+ *
+ * This process repeats until no more connections can be chosen. Either because
+ * they have been chosen previously or the choices made in the past have pre-determined
+ * the rest of the plan.
+ *
+ * E.g., if we choose `issue` as the outer loop, then `assignee` must be the inner loop
+ * since only two connections are involved.
+ */
 export class PlannerConnection implements PlannerNode {
   pinned: boolean;
   readonly #sort: Ordering;
