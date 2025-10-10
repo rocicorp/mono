2a29112ef5a96fff28a8960d181425315bb768d1
Gregory Baker <greg@roci.dev>
2025-10-10 14:25:52 -0700

    fixup

---
+++ apps/zbugs/db/migrations/0005_more-index-tweaks-for-all.sql
@@ -0,0 +1,2 @@
+CREATE INDEX "issue_projectID_assigneeID_modified_idx" ON "issue" USING btree ("projectID","assigneeID","modified","id");--> statement-breakpoint
+CREATE INDEX "issue_projectID_creatorID_modified_idx" ON "issue" USING btree ("projectID","creatorID","modified","id");
