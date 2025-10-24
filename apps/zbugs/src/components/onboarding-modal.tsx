1d9d926ccb228f505fc145588cac87c904154578
Alexander King <alexhking@gmail.com>
2025-10-24 16:27:29 -0400

    Edits to onboarding modal copy

--- apps/zbugs/src/components/onboarding-modal.tsx
+++ apps/zbugs/src/components/onboarding-modal.tsx
@@ -26,26 +26,17 @@ export function OnboardingModal({
       </p>
       <p>Things to try:</p>
       <h2>Clear cache and reload</h2>
+      <p>Zero's query-driven sync enables fast loads even from a cold cache.</p>
+      <h2>Instant interactions</h2>
       <p>
-        Gigabugs loads fast, even from a cold cache. Zero’s query-driven sync
-        puts you in complete control of what gets synced, and when.
-      </p>
-      <h2>Instant reads</h2>
-      <p>
-        Tap on anything. Choose any filter. Most interactions respond instantly.
-        Zero queries run locally first, and fallback to the server if necessary.
+        Click anything. Choose any filter. Create an issue or comment. Most
+        interactions respond instantly.
       </p>
       <h2>Infinite scroll</h2>
       <p>
         Perfectly buttery infinite scroll. Because it’s fun! (Or open and issue
         and hold down <span className="keyboard-keys">J</span> /{' '}
         <span className="keyboard-keys">K</span> 🏎️💨)
-      </p>
-      <h2>Instant writes</h2>
-      <p>
-        Create an issue or comment on an existing one (don’t worry about making
-        a mess, it’s test data). All mutations in Zero are client-first
-        automatically.
       </p>
       <h2>Live sync</h2>
       <p>Open two windows and watch changes sync between them.</p>
