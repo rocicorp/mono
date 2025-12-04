7cd84ed5719fb859a1c2a459eb6a533c98265e13
Matt Wonlaw <mlaw@roci.dev>
2025-12-04 16:45:35 -0500

    add special eqp method

--- packages/zqlite/src/explain-queries.ts
+++ packages/zqlite/src/explain-queries.ts
@@ -7,12 +7,9 @@ export function explainQueries(counts: RowCountsBySource, db: Database) {
     const queries = Object.keys(querySet);
     for (const query of queries) {
       const plan = db
-        // we should be more intelligent about value replacement.
-        // Different values result in different plans. E.g., picking a value at the start
-        // of an index will result in `scan` vs `search`. The scan is fine in that case.
-        .prepare(`EXPLAIN QUERY PLAN ${query.replaceAll('?', "'sdfse'")}`)
-        .all<{detail: string}>()
-        .map(r => r.detail);
+        .prepare(`EXPLAIN QUERY PLAN ${query}`)
+        .explainQueryPlan()
+        .map(r => (r as {detail: string}).detail);
       plans[query] = plan;
     }
   }
