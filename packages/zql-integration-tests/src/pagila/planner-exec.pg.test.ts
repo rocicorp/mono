0d2f2c8c51421e160c7478f4e38162e4a157078c
Matt Wonlaw <mlaw@roci.dev>
2025-12-11 11:37:17 -0500

    fix actual bug

--- packages/zql-integration-tests/src/pagila/planner-exec.pg.test.ts
+++ packages/zql-integration-tests/src/pagila/planner-exec.pg.test.ts
@@ -144,18 +144,15 @@ describe('Pagila planner execution cost validation', () => {
         .limit(100),
       validations: [
         ['correlation', -0.5],
-        // idk why this correlation is so off
-        // and why within optimal is so off.
+        // Correlation is bad for this one and we chose a non-optimal query.
         // What the below is saying is that the query as written is most optimal
         // (within-optimal and within-baseline are the same).
-        // On inspection of the data, however, the chosen plan (everything flipped) is definitely most optimal.
-        // ---
-        // The issue is that walking back up the tree is missing indices.
-        /*
-        1. payment table has NO index on rental_id - Only the primary key (payment_date, payment_id)
-        2. inventory table has NO direct index on film_id - The index is (store_id, film_id), which means film_id alone can't
-            use it efficiently
-        */
+        // On inspection of the data, however, the chosen plan (everything flipped) should be close to optimal.
+        // The issue is that walking back up the tree (from most nested exists check) is missing indices.
+        //
+        // 1. payment table has NO index on rental_id
+        // 2. inventory table has NO index on film_id
+        // We need to come up with a good way to penalize `SCAN` vs `SEARCH`
         ['within-optimal', 10],
         ['within-baseline', 10],
       ],
@@ -321,9 +318,9 @@ describe('Pagila planner execution cost validation', () => {
   ])(
     '$name',
     ({name, query, validations}) => {
-      if (name !== 'payment to film via rental chain (3 hops)') {
-        return;
-      }
+      // if (name !== 'payment to film via rental chain (3 hops)') {
+      //   return;
+      // }
       // Execute all plan attempts and collect results (baseline DB)
       const results = executeAllPlanAttempts(query, false, 40_000);
 
