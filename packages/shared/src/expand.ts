1e1bb87dd83be10fcbb3d6649cee562a5e738696
Chase Adams <gh@cadams.io>
2025-12-19 13:03:28 -0700

    chore: revert changes to expand

--- packages/shared/src/expand.ts
+++ packages/shared/src/expand.ts
@@ -1,12 +1,10 @@
 /**
  * Expand/simplifies a type for display in Intellisense.
  */
-export type Expand<T> = T extends infer O
-  ? {readonly [K in keyof O]: O[K]}
-  : never;
+export type Expand<T> = T extends infer O ? {[K in keyof O]: O[K]} : never;
 
 export type ExpandRecursive<T> = T extends object
   ? T extends infer O
-    ? {readonly [K in keyof O]: ExpandRecursive<O[K]>}
+    ? {[K in keyof O]: ExpandRecursive<O[K]>}
     : never
   : T;
