cbb09c8abd4befbcdd6eb3f60fbe55955dd958d1
Erik Arvidsson <arv@roci.dev>
2026-01-07 13:14:17 +0100

    fix(zero-server): Export executePostgresQuery

--- packages/zero-server/src/mod.ts
+++ packages/zero-server/src/mod.ts
@@ -1,13 +1,14 @@
 export * from '../../zero-protocol/src/application-error.ts';
 export * from '../../zql/src/mutate/custom.ts';
 export * from './custom.ts';
+export {executePostgresQuery} from './pg-query-executor.ts';
 export * from './process-mutations.ts';
 export * from './push-processor.ts';
 export * from './queries/process-queries.ts';
 export {
+  handleGetQueriesRequest,
   handleQueryRequest,
   handleTransformRequest,
-  handleGetQueriesRequest,
   type TransformQueryFunction,
 } from './queries/process-queries.ts';
-export * from './zql-database.ts';
+export {ZQLDatabase} from './zql-database.ts';
