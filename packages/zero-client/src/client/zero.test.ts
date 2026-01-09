6cb6cdecea32c41206f3702f58a578ae3d89f54f
Gregory Baker <greg@roci.dev>
2026-01-09 11:09:49 -0700

    test-name

--- packages/zero-client/src/client/zero.test.ts
+++ packages/zero-client/src/client/zero.test.ts
@@ -2455,7 +2455,7 @@ test.only('repro run-loop race', async () => {
   await z.waitForConnectionStatus(ConnectionStatus.Connected);
 });
 
-test.only('repro run-loop race 2', async () => {
+test.only('repro run-loop race using state.subscribe', async () => {
   const z = zeroForTest({auth: 'initial-token'});
 
   await z.triggerConnected();
