83996e000940760c5a2d71e329eee51dd5d7f67b
Matt Wonlaw <matt.wonlaw@gmail.com>
2026-01-14 12:31:41 -0500

    chore(ivm): add reasons to asserts (#5413)
    so we can debug more easily --
    https://discord.com/channels/830183651022471199/1460597200156622861/1461025806863175784

--- packages/zql/src/ivm/push-accumulated.ts
+++ packages/zql/src/ivm/push-accumulated.ts
@@ -278,7 +278,11 @@ export function mergeRelationships(left: Change, right: Change): Change {
         };
       }
       case 'edit': {
-        assert(right.type === 'edit');
+        assert(
+          right.type === 'edit',
+          () =>
+            `mergeRelationships: when left.type is edit and types match, right.type must be edit, got ${right.type}`,
+        );
         // merge edits into a single edit
         return {
           type: 'edit',
@@ -302,7 +306,11 @@ export function mergeRelationships(left: Change, right: Change): Change {
   }
 
   // left is always an edit here
-  assert(left.type === 'edit');
+  assert(
+    left.type === 'edit',
+    () =>
+      `mergeRelationships: when types differ, left.type must be edit, got left.type=${left.type}, right.type=${right.type}`,
+  );
   switch (right.type) {
     case 'add': {
       return {
