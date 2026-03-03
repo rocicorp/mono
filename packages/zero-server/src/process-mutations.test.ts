124191493641faf8274ccd5c95670e4984d2fb4a
Matt Wonlaw <mlaw@roci.dev>
2026-03-03 15:33:34 -0500

    fix type errors

--- packages/zero-server/src/process-mutations.test.ts
+++ packages/zero-server/src/process-mutations.test.ts
@@ -775,7 +775,13 @@ describe('handleMutateRequest', () => {
         recordedResults,
       } = createTrackingDatabase({
         // Fail only on the first transaction attempt for mutationID 2
-        [errorProviderKey]: ({mutationID, transactionCount}) =>
+        [errorProviderKey]: ({
+          mutationID,
+          transactionCount,
+        }: {
+          mutationID: number | undefined;
+          transactionCount: number;
+        }) =>
           mutationID === 2 && transactionCount === 2
             ? new Error(errorMsg)
             : undefined,
@@ -842,8 +848,11 @@ describe('handleMutateRequest', () => {
         recordedResults,
       } = createTrackingDatabase({
         // Fail both attempts for mutationID 2
-        [errorProviderKey]: ({mutationID}) =>
-          mutationID === 2 ? new Error(errorMsg) : undefined,
+        [errorProviderKey]: ({
+          mutationID,
+        }: {
+          mutationID: number | undefined;
+        }) => (mutationID === 2 ? new Error(errorMsg) : undefined),
       });
 
       const response = await handleMutateRequest(
