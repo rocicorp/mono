bf95af10dd499a93f8121717797fecc50dc43008
Erik Arvidsson <erik.arvidsson@gmail.com>
2026-04-14 14:11:00 +0200

    change enum order

--- packages/zql/src/ivm/change-type-enum.ts
+++ packages/zql/src/ivm/change-type-enum.ts
@@ -1,9 +1,9 @@
 export const ADD = 0;
 export const REMOVE = 1;
-export const CHILD = 2;
-export const EDIT = 3;
+export const EDIT = 2;
+export const CHILD = 3;
 
 export type ADD = typeof ADD;
 export type REMOVE = typeof REMOVE;
-export type CHILD = typeof CHILD;
 export type EDIT = typeof EDIT;
+export type CHILD = typeof CHILD;
