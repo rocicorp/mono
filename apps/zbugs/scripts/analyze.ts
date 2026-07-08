bb4b62b0380c78a9c830cc0e216a9cff264fb69b
Greg Baker <greg@roci.dev>
2026-07-08 16:47:06 -0700

    test

--- apps/zbugs/scripts/analyze.ts
+++ apps/zbugs/scripts/analyze.ts
@@ -3,5 +3,4 @@ import '../../../packages/shared/src/dotenv.ts';
 import { runAnalyzeCLI } from '../../../packages/zero/src/analyze.ts';
 import { schema } from '../shared/schema.ts';
 
-await runAnalyzeCLI({ schema });
-console.log('test2');
+await runAnalyzeCLI({ schema }); // test
