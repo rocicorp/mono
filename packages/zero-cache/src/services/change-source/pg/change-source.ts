ae6f11d7abad23841208928f66fe5fac5247b24d
Greg Baker <greg@roci.dev>
2026-09-13 22:02:35 -0700

    perf(zero-cache): implement native postgresql binary logical replication
    (cherry picked from commit b69d7f96387b315e259b5ad9d5529ce0301bc093)

--- packages/zero-cache/src/services/change-source/pg/change-source.ts
+++ packages/zero-cache/src/services/change-source/pg/change-source.ts
@@ -145,6 +145,7 @@ export async function initializePostgresChangeSource(
   {backupV5}: ReplicaOptions = {backupV5: true},
   purgeLock?: PurgeLock | null,
   streamInboundTimeoutMs?: number | undefined,
+  textReplication?: boolean | undefined,
 ): Promise<InitializeResult> {
   const db = await connectPgClient(lc, upstreamURI, 'change-source-init');
   try {
@@ -221,6 +222,8 @@ export async function initializePostgresChangeSource(
       lagReportIntervalMs,
       syncOptions.textCopy,
       streamInboundTimeoutMs,
+      undefined,
+      textReplication,
     );
 
     const destinationBackupURL =
@@ -384,6 +387,7 @@ export class PostgresChangeSource implements ChangeSource {
   readonly #context: ServerContext;
   readonly #lagReporter: LagReporter | null;
   readonly #textCopy: boolean;
+  readonly #textReplication: boolean;
   readonly #streamInboundTimeoutMs: number | undefined;
   readonly #subscribe: typeof subscribe;
   readonly #streamBackfill: typeof streamBackfill;
@@ -405,6 +409,7 @@ export class PostgresChangeSource implements ChangeSource {
       subscribe?: typeof subscribe;
       streamBackfill?: typeof streamBackfill;
     } = {},
+    textReplication?: boolean | undefined,
   ) {
     this.#lc = lc.withContext('component', 'change-source');
     this.#subscribe = deps.subscribe ?? subscribe;
@@ -420,6 +425,7 @@ export class PostgresChangeSource implements ChangeSource {
     this.#backupOptions = backupOptions;
     this.#context = context;
     this.#textCopy = textCopy ?? false;
+    this.#textReplication = textReplication ?? false;
     this.#streamInboundTimeoutMs = streamInboundTimeoutMs;
     this.#lagReporter =
       lagReportIntervalMs > 0
@@ -505,6 +511,7 @@ export class PostgresChangeSource implements ChangeSource {
       undefined,
       undefined,
       this.#streamInboundTimeoutMs,
+      !this.#textReplication,
     );
     const acker = new Acker(acks);
 
